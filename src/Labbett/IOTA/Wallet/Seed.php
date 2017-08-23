b792b5ac9ecb451970f134648833db46d2a81d48
Patrick Labbett <patrick.labbett@gmail.com>
2017-08-23 23:41:46 +0000

    Apply fixes from StyleCI

--- src/Labbett/IOTA/Wallet/Seed.php
+++ src/Labbett/IOTA/Wallet/Seed.php
@@ -1,24 +1,25 @@
 <?php
+
 namespace Labbett\IOTA\Wallet;
 
 class Seed
 {
-	public static function generate()
-	{
-		$seed = '';
+    public static function generate()
+    {
+        $seed = '';
 
-		$allowed_characters = [
-			'A','B','C','D','E','F','G','H','I',
-			'J','K','L','M','N','O','P','Q','R',
-			'S','T','U','V','W','X','Y','Z','9',
-		];
+        $allowed_characters = [
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+            'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '9',
+        ];
 
-		for( $i = 0; $i < 81; $i++ )
-		{
-			// Cryptographically secure. (7.1 + built in)
-			// http://php.net/manual/en/function.random-int.php
-			$seed .= $allowed_characters[ random_int(0, count( $allowed_characters) - 1 ) ];
-		}
-		return $seed;
-	}
+        for ($i = 0; $i < 81; $i++) {
+            // Cryptographically secure. (7.1 + built in)
+            // http://php.net/manual/en/function.random-int.php
+            $seed .= $allowed_characters[random_int(0, count($allowed_characters) - 1)];
+        }
+
+        return $seed;
+    }
 }
